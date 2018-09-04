0c93281ccdc86716f38b3ba2d1bc3e15808d1423
Yogesh Sonawane <yogesh-sonawane@users.noreply.github.com>
2018-09-04 18:01:06 +0530

    changed server

--- app-config/db-config.js
+++ app-config/db-config.js
@@ -1,29 +1,37 @@
 var Mongoose = require('mongoose');
-var DbSetting = function() {
+var DbSetting = function () {
   var dbConfig = {
-    dbUrl: 'mongodb://127.0.0.1:27017/flokapture-server',
+    dbUrl: 'mongodb://173.248.135.123:27017/flokapture-server',
   };
 
   return dbConfig;
 };
 exports.dbSetting = DbSetting;
 
-exports.dbServer = function() {
-  var serverUrl = 'mongodb://127.0.0.1:27017/flokapture-server';
-  Mongoose.Promise = global.Promise;
-  var dbServer = Mongoose.createConnection(serverUrl);
-  return dbServer;
+exports.dbServer = function () {
+  try {
+    var serverUrl = 'mongodb://173.248.135.123:27017/flokapture-server';
+    Mongoose.Promise = global.Promise;
+    var dbServer = Mongoose.createConnection(serverUrl);
+    return dbServer;
+  } catch (exception) {
+    console.log(exception);
+  }
 };
 
-exports.apiAddress = function() {
+exports.apiAddress = function () {
   var apiBaseAddress = 'http://localhost:3000/api/';
   return apiBaseAddress;
 };
 
-exports.mongoDbServer = function() {
+exports.mongoDbServer = function () {
   var Db = require('mongodb').Db;
   var MongoClient = require('mongodb').MongoClient;
   var Server = require('mongodb').Server;
 
-  return {Db: Db, Server: Server, Client: MongoClient};
+  return {
+    Db: Db,
+    Server: Server,
+    Client: MongoClient
+  };
 };
